58e5ec2e1c1ac70450093e6ea9fc6c2b9c8ef1e0
MayPhyoKyaw <MayPhyoKyaw@users.noreply.github.com>
2021-05-29 14:36:19 +0630

    repair add item

--- public/js/db.js
+++ public/js/db.js
@@ -66,60 +66,61 @@ $(document).ready(function() {
     add.addEventListener('click', function (e) {
       var itemName = document.getElementById("toDo").value;
         console.log('button was clicked');
-        var li = document.createElement("li");
-        var t = document.createTextNode(itemName);
-        li.appendChild(t);
+        // var li = document.createElement("li");
+        // var t = document.createTextNode(itemName);
+        // li.appendChild(t);
         if (itemName === '') {
             alert("You must write something!");
         } else {
-            document.getElementById("toDoList").appendChild(li);
+            // document.getElementById("toDoList").appendChild(li);
+            fetch("/addToDoItem", {
+              method: 'POST',
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                item_name: itemName,
+              })
+            })
+              .then(function (response) {
+                console.log(response)
+                if (response.ok) {
+                  console.log('clicked!!');
+                  return;
+                }
+                throw new Error('Failed!!');
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+              location.reload();
         }
-        document.getElementById("toDo").value = "";
+        // document.getElementById("toDo").value = "";
 
-        var itemID = document.getElementById("itemId").innerHTML;
-        var idSpan = document.createElement("span");
-        idSpan.className = "hideText";
-        var idText = document.createTextNode(itemID);
-        idSpan.appendChild(idText);
-        var closeSpan = document.createElement("SPAN");
-        // var txt = document.createTextNode("\u00D7");
-        closeSpan.className = "close";
-        var closeIcon = document.createElement("I");
-        closeIcon.className = "fa fa-close";
-        closeSpan.appendChild(closeIcon);
-        li.appendChild(closeSpan);
+        // var itemID = document.getElementById("itemId").innerHTML;
+        // var idSpan = document.createElement("span");
+        // idSpan.className = "hideText";
+        // var idText = document.createTextNode(itemID);
+        // idSpan.appendChild(idText);
+        // var closeSpan = document.createElement("SPAN");
+        // // var txt = document.createTextNode("\u00D7");
+        // closeSpan.className = "close";
+        // var closeIcon = document.createElement("I");
+        // closeIcon.className = "fa fa-close";
+        // closeSpan.appendChild(closeIcon);
+        // li.appendChild(closeSpan);
 
-        var editSpan = document.createElement("SPAN");
-        // var txt = document.createTextNode("\u00D7");
-        editSpan.className = "edit";
-        var editIcon = document.createElement("I");
-        editIcon.className = "fa fa-edit";
-        editSpan.appendChild(editIcon);
-        li.appendChild(editSpan);
+        // var editSpan = document.createElement("SPAN");
+        // // var txt = document.createTextNode("\u00D7");
+        // editSpan.className = "edit";
+        // var editIcon = document.createElement("I");
+        // editIcon.className = "fa fa-edit";
+        // editSpan.appendChild(editIcon);
+        // li.appendChild(editSpan);
   
-        if(itemName !== ""){
-          fetch("/addToDoItem", {
-            method: 'POST',
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              item_name: itemName,
-            })
-          })
-            .then(function (response) {
-              console.log(response)
-              if (response.ok) {
-                console.log('clicked!!');
-                return;
-              }
-              throw new Error('Failed!!');
-            })
-            .catch(function (error) {
-              console.log(error);
-            });
-        }
+        // if(itemName !== ""){
+        // }
     });
 
     // Add a "checked" symbol when clicking on a list item
